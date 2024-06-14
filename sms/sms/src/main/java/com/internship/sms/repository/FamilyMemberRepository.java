d3cb70fee007f859004d393eafe4cdf06ae914dc
DESKTOP-KURPU16\Dell <chawsuhh562@gmail.com>
2024-06-14 17:21:52 +0630

    no message

---
+++ sms/sms/src/main/java/com/internship/sms/repository/FamilyMemberRepository.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.internship.sms.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.internship.sms.entity.FamilyMember;
+
+/**
+ * 
+ */
+public interface FamilyMemberRepository extends JpaRepository<FamilyMember, Long> {
+
+}
