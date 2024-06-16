03861366039290a1a5b18fbeb215cd62fa078cba
Thu Soe San <thusoesan69@gmail.com>
2024-06-16 20:53:41 +0630

    no message

--- sms/sms/src/main/java/com/internship/sms/repository/SubjectRepository.java
+++ sms/sms/src/main/java/com/internship/sms/repository/SubjectRepository.java
@@ -3,9 +3,22 @@
  */
 package com.internship.sms.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.internship.sms.common.ActiveStatus;
+import com.internship.sms.entity.Subject;
+
+
 /**
  * 
  */
-public interface SubjectRepository {
-
+@Repository
+public interface SubjectRepository extends JpaRepository<Subject, Long>{
+	@Query("select u from Subject u where u.activeStatus = :activeStatus")
+	  List<Subject> getAllByActiveStatus(@Param("activeStatus")ActiveStatus activeStatus);
 }
---
+++ sms/sms/src/main/java/com/internship/sms/service/SubjectService.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.internship.sms.service;
+
+import java.util.List;
+
+import com.internship.sms.entity.Subject;
+
+/**
+ * 
+ */
+public interface SubjectService {
+
+	public List<Subject> getAll();
+	
+	public Subject getSubjectById(Long id);
+	
+	public Subject create(Subject subject);
+	
+	public Subject update(Subject subject);
+	
+	public boolean delete(Subject subject);
+	
+}
