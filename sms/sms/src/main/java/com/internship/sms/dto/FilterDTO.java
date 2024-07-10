b471337088d33b15c86928575edb40e33a06f11b
DESKTOP-KURPU16\Dell <chawsuhh562@gmail.com>
2024-07-10 11:33:59 +0630

    filterDto

---
+++ sms/sms/src/main/java/com/internship/sms/dto/FilterDTO.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.internship.sms.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 
+ */
+@Getter
+@Setter
+public class FilterDTO {
+
+	private Long batchId;
+
+	private Long semesterId;
+
+	private String major;
+
+}
