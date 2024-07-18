df889c6fee886610d7cce214eeb64c1f7257c369
Khun Maung Ngwee <khunmgngwe@ucstgi.edu.mm>
2024-07-18 10:06:11 +0630

    Notice CRUD

---
+++ sms/sms/src/main/java/com/internship/sms/repository/NoticeRepository.java
@@ -0,0 +1,18 @@
+package com.internship.sms.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import com.internship.sms.common.ActiveStatus;
+import com.internship.sms.entity.Notice;
+
+@Repository
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+
+	@Query("select u  from Notice u where u.activeStatus=:activeStatus")
+	List<Notice> getAllByActiveStatus(@Param("activeStatus") ActiveStatus activeStatus);
+
+}
