7c7f34f8b0b166a50489f0ac2a18b6675ed0d64f
Thu Soe San <thusoesan69@gmail.com>
2024-07-25 13:45:36 +0630

    no message

--- sms/sms/src/main/java/com/internship/sms/entity/Section.java
+++ sms/sms/src/main/java/com/internship/sms/entity/Section.java
@@ -2,8 +2,6 @@ package com.internship.sms.entity;
 
 import java.io.Serializable;
 import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
